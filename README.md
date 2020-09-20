d4e67b9b99621f4e0427486bbeb50df0240003f1
Xiaoyang Ma <boltma@gmail.com>
2020-09-20 13:38:59 +0800

    Add report

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MIPS-pipeline-CPU
-Course experiment for THUEE Fundamental of Digital Logic and Processor 2020
+A simple MIPS pipeline CPU as course experiment for THUEE Fundamental of Digital Logic and Processor 2020
+
+Using Xilinx Basys-3, xc7a35tcpg236-1
+
+- Maximum Frequency: 102.2MHz
+- CPI on quicksort: 1.243
+
+See report for more details.
+
+Other course experiments for Fundamental of Digital Logic and Processor 2020 could be seen [here](https://github.com/boltma/Digital-Logic-and-Processor).
--- assembly/rand_gen.py
+++ assembly/rand_gen.py
@@ -1,9 +1,16 @@
+import array
+import binascii
 import random
 
 with open('data.txt', 'w') as out:
-    with open('data_memory.txt', 'w') as mem_out:
-        for i in range(128):
-            num = random.getrandbits(32)
-            hex_num = '{:08x}'.format(num)
-            out.write(f'{hex_num}\n')
-            mem_out.write('RAM_data[%d] <= 32\'h%s;\n' % (i, hex_num))
+	with open('data_memory.txt', 'w') as mem_out:
+		with open('data.in', 'wb') as data_out:
+			for i in range(128):
+				num = random.getrandbits(32)
+				hex_num = '{:08x}'.format(num)
+				bin_num = binascii.unhexlify(hex_num)
+				binswap = array.array('L', bin_num)
+				binswap.byteswap()
+				data_out.write(binswap)
+				out.write(f'{hex_num}\n')
+				mem_out.write('RAM_data[%d] <= 32\'h%s;\n' % (i, hex_num))
